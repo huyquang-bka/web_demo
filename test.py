4fab0ad1532b32025da3490b8884087e862d2b49
Quang Nguyen Huy <huyquangbka@gmail.com>
2023-02-14 08:27:42 +0700

    add test flask

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-import os
+from flask import Flask, render_template
 
-path = r"\\PC198\output"
 
-print(os.listdir(path))
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8501, debug=True)
